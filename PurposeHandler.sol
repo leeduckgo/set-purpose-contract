867fe494713822f8f88dc666e1ae090877a197d2
leeduckgo <albertschr@163.com>
2021-10-29 08:00:09 +0800

    feat/add sender checker

--- PurposeHandler.sol
+++ PurposeHandler.sol
@@ -2,19 +2,21 @@ pragma solidity >=0.8.0 <0.9.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-//import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-contract PurposeHandler {
+contract YourContract {
 
   //event SetPurpose(address sender, string purpose);
 
-  string public purpose = "Building Unstoppable Apps!!!";
+  string public purpose = "Building Unstoppable Apps";
+  address public owner;
 
   constructor() {
+    owner = msg.sender;
     // what should we do on deploy?
   }
 
   function setPurpose(string memory newPurpose) public {
+      require( msg.sender == owner, "NOT THE OWNER!");
       purpose = newPurpose;
       console.log(msg.sender,"set purpose to",purpose);
       //emit SetPurpose(msg.sender, purpose);
